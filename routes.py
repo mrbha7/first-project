a2a1c013586a4f776820074f2cfd8e1889f005b0
Bhaivsh Bhattar <bhabhavish@gmail.com>
2017-11-22 20:37:44 -0600

    stuff

--- routes.py
+++ routes.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template #import class and function
-
+from models import db #import db variable
 app = Flask(__name__) #creates instance of flask class
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask' #used to attach database to application look at flasksqlalchemy page for more info
+db.init_app(app) #initialize app to use this setup
 
 @app.route("/") #map this slash to the function index
 def index():
