1a9c366f7c442a917621f67c0687e619d1e37a7a
mrbha7 <bhabhavish@gmail.com>
2017-12-23 15:19:32 -0600

    Add files via upload

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy #uses extention in this file
+from flask.ext.sqlalchemy import SQLAlchemy #uses extention in this file
 from werkzeug import generate_password_hash, check_password_hash
+
+import geocoder
+import urllib2
+import json
 
 db = SQLAlchemy() #creates database variable
 
@@ -22,3 +26,48 @@ class User(db.Model): #creates python class to model columns in the table
 
     def check_password(self,password): # used to check password
         return check_password_hash(self.pwdhash, password)
+
+class Place(object):
+    def meters_to_walking_time(self,meters):
+        # 84 meters is one minute walking time
+        return int(meters/ 80)
+
+    def wiki_path(self, slug):
+        return urllib2.urlparse.urljoin("http://en.wikipedia.org/wiki/",slug.replace(' ','_'))
+
+    def address_to_latlng(self,address):
+        g = geocoder.google(address)
+        return (g.lat, g.lng)
+
+    def query(self, address): #converts address to lat and lng
+        lat, lng = self.address_to_latlng(address)
+
+        #constructs a url using the lat, lng
+        query_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'.format(lat, lng)
+        g = urllib2.urlopen(query_url)
+        results = g.read()
+        g.close()
+
+        data = json.loads(results)
+
+        places = [] #makes a python dictionary
+        for place in data['query']['geosearch']:
+            name = place['title']
+            meters = place['dist']
+            lat = place['lat']
+            lng = place['lon']
+
+            wiki_url = self.wiki_path(name)
+            walking_time = self.meters_to_walking_time(meters)
+
+            d = {
+                'name': name,
+                'url': wiki_url,
+                'time': walking_time,
+                'lat': lat,
+                'lng': lng
+            }
+
+            places.append(d)
+            #return list
+        return places
